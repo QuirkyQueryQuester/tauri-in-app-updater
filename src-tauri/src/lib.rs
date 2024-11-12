2a71b45dc90c61ebcf4064a083bc5daf263823bc
Lorenz <lorenz@misax.at>
2024-11-12 07:28:07 +0100

    changes...

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,10 +8,10 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_updater::Builder::default()
-            .pubkey("dW50cnVzdGVkIGNvbW1lbnQ6IG1pbmlzaWduIHB1YmxpYyBrZXk6IDU2QzZFOTI2M0VCMDYyODYKUldTR1lyQStKdW5HVnZKNW42eDFYTTdWUzNZdWRJUkJGODZHeDRVSGx3NHk5aFlXSmEzeHp1dXQK")
+        .plugin(tauri_plugin_updater::Builder::new()
             .build())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
 import {invoke} from "@tauri-apps/api/core";
-import {check} from '@tauri-apps/plugin-updater';
+import {check, Update} from '@tauri-apps/plugin-updater';
 import {ask, message} from '@tauri-apps/plugin-dialog';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {relaunch} from "@tauri-apps/plugin-process";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, RouterOutlet],
+    imports: [CommonModule, RouterOutlet, HttpClientModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
 export class AppComponent {
+    private http = inject(HttpClient)
+
     greetingMessage = "";
 
     greet(event: SubmitEvent, name: string): void {
@@ -25,7 +29,8 @@ export class AppComponent {
     }
 
     async checkForUpdates(onUserClick = false) {
-        const update = await check();
+        const update = await check()
+
 
         if (update === null) {
             await message('Failed to check for updates.\nPlease try again later.', {
@@ -35,6 +40,9 @@ export class AppComponent {
             });
             return;
         } else if (update?.available) {
+            let downloaded = 0;
+            let contentLength: number|undefined = 0;
+
             const yes = await ask(`Update to ${update.version} is available!\n\nRelease notes: ${update.body}`, {
                 title: 'Update Available',
                 kind: 'info',
@@ -44,8 +52,38 @@ export class AppComponent {
 
             if (yes){
                 console.log('INSTALL UPDATES')
+                await update.downloadAndInstall((ev) => {
+                    console.log(ev)
+                    switch (ev.event){
+                        case "Started":
+                            contentLength = ev.data.contentLength;
+                            console.log(`started downloading ${ev.data.contentLength} bytes`);
+                            break;
+                        case "Progress":
+                            downloaded += ev.data.chunkLength;
+                            console.log(`downloaded ${downloaded} from ${contentLength}`);
+                            break;
+                        case "Finished":
+                            console.log('Download finished');
+                            break;
+                    }
+                });
+
+                await relaunch();
             }
+        } else if (onUserClick){
+            await message('You are on the latest version.', {
+                title: 'No Update Available',
+                kind: 'info',
+                okLabel: 'OK'
+            })
         }
 
+    }
+
+    async checkUrl() {
+        this.http.get('https://gist.githubusercontent.com/QuirkyQueryQuester/6f2e72bfc171cc2f4709c8584eac3a9c/raw/latest.json').subscribe(res => {
+            console.log(res)
+        })
     }
 }
